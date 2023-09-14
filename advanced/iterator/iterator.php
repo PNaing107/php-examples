aa4aa22c3ac45d77b675f3ad6951f1f81fdc8233
Phone Naing <phone.naing.dev@gmail.com>
2023-09-14 22:22:54 +0100

    add filter iterator

--- advanced/iterator/iterator.php
+++ advanced/iterator/iterator.php
@@ -10,13 +10,27 @@ class BasicIterator extends IteratorIterator
         // Setup the inner SplFileObject's properties to process CSV
         $file = $this->getInnerIterator();
         $file->setFlags(SplFileObject::READ_CSV);
-        $file->setCsvControl(',','"',"\\");
+        $file->setCsvControl(',', '"', "\\");
+    }
+}
 
+class FilterRows extends FilterIterator
+{
+    public function accept()
+    {
+        $current = $this->getInnerIterator()->current();
+
+        if (count($current) == 1) {
+            return false;
+        }
+
+        return true;
     }
 }
 
 $filePath = './data.csv';
 $iterator = new BasicIterator($filePath);
-foreach($iterator as $i => $row) {
+$iterator = new FilterRows($iterator); // we have chained the FilterRows iterator on to the BasicIterator
+foreach ($iterator as $i => $row) {
     var_dump($row);
 }
